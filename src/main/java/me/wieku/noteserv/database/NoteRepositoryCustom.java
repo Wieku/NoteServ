3ebaa7c258d5b5a043a9f1ef6af5ccd1e805aeb2
Sebastian Krajewski <mail@wieku.me>
2018-12-02 02:48:16 +0100

    Fix import

--- src/main/java/me/wieku/noteserv/database/NoteRepositoryCustom.java
+++ src/main/java/me/wieku/noteserv/database/NoteRepositoryCustom.java
@@ -1,7 +1,5 @@
 package me.wieku.noteserv.database;
 
-import me.wieku.noteserv.view.NoteView;
-
 public interface NoteRepositoryCustom {
     Note getNewestNote(long noteId);
     Note getNoteRevision(long noteId, long revision);
